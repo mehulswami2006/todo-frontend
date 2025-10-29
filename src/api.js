2bbe9817973db978fca9318244a0d62d35dceee9
mehulswami2006 <mehulswami2006@gmail.com>
2025-10-29 13:43:15 +0530

    Update API URL to production endpoint

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API = process.env.REACT_APP_API_URL || 'https://todo-backend-9yyn.onrender.com';
 
 
 export const authFetch = (path, token, opts = {}) => {
